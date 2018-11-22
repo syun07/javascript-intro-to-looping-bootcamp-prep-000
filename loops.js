0c46be5e42e80cfada887f6029cc70f866b17890
Sarah Yun <sarahyun7@gmail.com>
2018-11-22 05:42:55 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -25,12 +25,12 @@ function doWhileLoop(array) {
 }
   
   
-// function doWhileLoop(array)  
-//   function maybeTrue() {
-//     return Math.random() >= 0.5
-//     }
-//   do {
-//     array.pop();
-//   } while (array.length > 0 && maybeTrue())
-//   return array
-// }
+function doWhileLoop(array)  
+  function maybeTrue() {
+    return Math.random() >= 0.5
+    }
+  do {
+    array.pop();
+  } while (array.length > 0 && maybeTrue())
+  return array
+}
