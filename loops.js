d1d68cb337d339667374450f85cf635de092094c
Sarah Yun <sarahyun7@gmail.com>
2018-11-22 05:38:30 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -18,13 +18,13 @@ function whileLoop(n) {
 
 function doWhileLoop(array) {
   var i = 0;
-  function incrementVar() {
+  function incrementVariable() {
     i = i + 1;
   }
   
   do {
     array.pop();
-  } while (array.length > 0 && incrementVar);
+  } while (array.length > 0 && incrementVariable);
 }
   
   
